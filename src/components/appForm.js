27ac903e06b02a352f36f52b18ed573234bfea2c
Akiva Cohen <akiva.coen@gmail.com>
2023-08-12 09:33:12 +0300

    first commit

---
+++ src/components/appForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateForm from "./createForm";
+
+function Form() {
+  return (
+    <div className="">
+      <CreateForm />
+    </div>
+  );
+}
+
+export default Form;
